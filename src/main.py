c638b2cb6375d99181e37f2da4908dd1c12c67dd
DocOcassi <dococassi@boukom.com>
2015-06-20 13:14:42 +0100

    Try a flask app

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
 
